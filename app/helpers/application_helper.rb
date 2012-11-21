5e662aaa06a1d5abbf757eac770e3fe7710c6445
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-11-22 05:58:52 +0900

    add title method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  BASE_TITLE = "Ruby on Rails Tutorial Sample App".freeze
+  # Return a title on a per-page basis.
+  def title
+    if @title.presence
+      "#{BASE_TITLE} | #@title"
+    else
+      BASE_TITLE
+    end
+  end
 end
