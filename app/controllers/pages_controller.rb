30be84ca52a050a94f03dd7cc882f959ab33b074
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-11-21 22:58:36 +0900

    define title method and @title in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,18 @@
 class PagesController < ApplicationController
+
+  def title
+    @title
+  end
+
   def home
+    @title = __method__.to_s.capitalize
   end
 
   def contact
+    @title = __method__.to_s.capitalize
   end
 
   def about
+    @title = __method__.to_s.capitalize
   end
 end
