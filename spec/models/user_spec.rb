8073980dd1a0ab53d7d9460e15e18dcee9235341
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-06 06:10:59 +0900

    add validation test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user_attr = { :name => "Example User", :email => "user@example.com" }
+  end
+
+  it 'should not raise error when creating one with a correct attributes' do
+    expect { User.create! @user_attr }.to_not raise_error ActiveRecord::RecordInvalid
+  end
+
+  it 'should require a name' do
+    User.new( @user_attr.merge name: '' ).should_not be_valid
+  end
 end
