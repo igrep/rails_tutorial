73b7de5d85823d8d752750ca2a86d71829cd5e02
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-12-07 06:14:51 +0900

    add 2 validations: user name length and email format, and add add UserSpecHelper with +new_user_with method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,15 @@
 
 require 'spec_helper'
 
+module UserSpecHelper
+  def new_user_with params
+    User.new( @user_attr.merge params )
+  end
+end
+
 describe User do
+  include UserSpecHelper
+
   before :all do
     @user_attr = { :name => "Example User", :email => "user@example.com" }
   end
@@ -22,7 +30,27 @@ describe User do
 
   [:name, :email].each do|required_field|
     it "should require a #{required_field}" do
-      User.new( @user_attr.merge required_field => '' ).should_not be_valid
+      new_user_with( required_field => '' ).should_not be_valid
+    end
+  end
+
+  it 'should reject too long name' do
+    new_user_with( name: 'a' * 51 ).should_not be_valid;
+  end
+
+  it 'should accept a user whose name is as long as 50' do
+    new_user_with( name: 'a' * 50 ).should be_valid;
+  end
+
+  it 'should accept valid email addresses' do
+    %w[user@acme.com THE_USER@valid.org first.last@example.jp].each do|address|
+      new_user_with( email: address ).should be_valid
+    end
+  end
+
+  it 'should reject invalid email addresses' do
+    %w[user@acme,com user_at_invalid.org user@example.].each do|address|
+      new_user_with( email: address ).should_not be_valid
     end
   end
 end
